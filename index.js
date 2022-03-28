20169a14371b4f83377a69b01d6c9341ccb44456
Alexandru Pavel <alex.pavel2002@gmail.com>
2022-03-28 15:42:04 +0300

    Etapa 4 1. complet

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const sharp = require("sharp");
 const app = express();
 app.set("view engine", "ejs");
 
-app.use("/resurse", express.static(__dirname+"/resurse"));
+app.use("/resurse", express.static(__dirname + "/resurse"));
 
 app.get(["/", "/index", "/home"], function (req, res) {
     res.render("pagini/index", {ip: req.ip});
@@ -13,28 +13,55 @@ app.get(["/", "/index", "/home"], function (req, res) {
 });
 
 app.get("/*.ejs", function(req, res) {
-    res.status(403).render("pagini/403");
+    randeazaEroare(res, 403)
+    res.end();
+});
+
+app.get("/eroare", function(req, res) {
+    randeazaEroare(res, 1, "Titlu schimbat");
     res.end();
 });
 
 app.get("/*", function(req, res) {
-    res.render("pagini"+req.url, function(err, rezRender) {
-        if(err) {
-            if(err.message.includes("Failed to lookup view")) {
-                console.log(err);
-                res.status(404).render("pagini/404");
+    try {
+        res.render("pagini" + req.url, function(err, rezRender) {
+            if(err) {
+                if(err.message.includes("Failed to lookup view")) {
+                    console.log(err);
+                    randeazaEroare(res, 404);
+                }
+                else {
+                    randeazaEroare(res, "Ai busit ceva la site");
+                }
             }
             else {
-                res.render("pagini/eroare_generala");
+                console.log(rezRender);
+                res.send(rezRender);
             }
-        }
-        else {
-            console.log(rezRender);
-            res.send(rezRender);
-        }
-    });
+        });
+    }
+    catch {
+        randeazaEroare(res, 404);
+    }
     res.end();
 });
+
+function creeazaErori() {
+    var buf = fs.readFileSync(__dirname + "/resurse/json/erori.json").toString("utf8");
+    obErori = JSON.parse(buf);
+}
+creeazaErori();
+
+function randeazaEroare(res, identificator, titlu, text, imagine) {
+    var eroare = obErori.erori.find(function (elem) {return elem.identificator === identificator;});
+    titlu = titlu || (eroare && eroare.titlu) || "Titlu eroare custum";
+    text = text || (eroare && eroare.text) || "Text eroare custum";
+    imagine = imagine || (eroare && (obErori.cale_baza + "/" + eroare.imagine)) || "Imagine eroare custum";
+    if(eroare && eroare.status)
+        res.status(eroare.identificator);
+    res.render("pagini/eroare_generala", {titlu: titlu, text: text, imagine: imagine});
+    res.end();
+}
 
 app.listen(8080);
 console.log("A pornit!");
